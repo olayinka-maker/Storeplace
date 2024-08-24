126a7849bfd79a98b43c499a4af74ca6a19aa8cf
olayinka-maker <ajibodeolayinka09@gmail.com>
2024-08-24 02:58:03 +0100

    working on the modals

--- storeplace/app/(root)/page.tsx
+++ storeplace/app/(root)/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { Modal } from "@/components/modals";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function SetUpPage() {
-  return (
-    <main>
-      <Modal
-        isOpen
-        title="test"
-        description=" description test"
-        onClose={() => {}}
-      >
-        childrens
-      </Modal>
-    </main>
-  );
+  const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.onOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <main>Root page</main>;
 }
--- storeplace/components/modals/store-modals.tsx
+++ storeplace/components/modals/store-modals.tsx
@@ -1,8 +1,41 @@
+"use client";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../modals";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  storename: z.string().min(2, {
+    message: "Storename must be at least 2 characters.",
+  }),
+});
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values);
+}
+
 export const StoreModals = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      storename: "",
+    },
+  });
 
   return (
     <Modal
@@ -11,8 +44,36 @@ export const StoreModals = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      {" "}
-      Future Create Store Forms
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="storename"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Storename</FormLabel>
+                <FormControl>
+                  <Input placeholder="E-commerce StoreName" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className=" flex   gap-4 justify-end">
+            <Button
+              className=" "
+              variant={"outline"}
+              onClick={storeModal.onClose}
+            >
+              Cancle
+            </Button>
+            <Button type="submit">Continue</Button>
+          </div>
+        </form>
+      </Form>
     </Modal>
   );
 };
